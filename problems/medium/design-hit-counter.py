1cc5ba45bda670e66f9ad2af8f845ff6b0d2b0de
Jacob Dahan <jbdahan@gmail.com>
2025-12-12 23:30:57 -0800

    Redo: 362. Design Hit Counter (optimized)

--- problems/medium/design-hit-counter.py
+++ problems/medium/design-hit-counter.py
@@ -9,26 +9,36 @@ class HitCounter:
     # Store it as such to make `get` calls fast.
     # Appending to the list is O(1) amortized and requires O(N) memory.
     def hit(self, timestamp: int) -> None:
-        self._counter.append(timestamp)
+        if not self._counter:
+            self._counter.append((timestamp, 1))
+            return
+
+        ts, c = self._counter[-1]
+        if ts == timestamp:
+            self._counter[-1] = (ts, c + 1)
+            return
+        
+        self._counter.append((timestamp, 1))
 
     # Returns the number of hits in the past 5 minutes from timestamp (i.e., the past 300 seconds).
     # Specifically, for timestamp N, only hits at timestamp M (M >= N - 300) are included.
     def getHits(self, timestamp: int) -> int:
         left, right = 0, len(self._counter) - 1
-        target_ts = timestamp - 300 # 1
-
+        target_ts = timestamp - 300
         while left <= right:
-            idx = (right - left) // 2 + left # 0
-            ts = self._counter[idx] # 1
+            idx = (right - left) // 2 + left
+            ts, _ = self._counter[idx]
             if ts <= target_ts:
-                # Timestamp is outside of or equal to the window, look to the right
                 left = idx + 1
             else:
-                # Timestamp is within the window, look to the left
                 right = idx - 1
-        
-        return len(self._counter) - left 
 
+        sum = 0
+        while left < len(self._counter):
+            sum += self._counter[left][1]
+            left += 1
+
+        return sum
 
 # Your HitCounter object will be instantiated and called as such:
 # obj = HitCounter()
