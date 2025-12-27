4629269258432627bbec6c34b9ae08229a73e54b
Jacob Dahan <jbdahan@gmail.com>
2025-12-27 09:25:43 -0800

    Refactored: 102. Binary Tree Level Order Traversal

--- problems/medium/binary-tree-level-order-traversal.py
+++ problems/medium/binary-tree-level-order-traversal.py
@@ -8,25 +8,34 @@ from collections import deque
 #         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        level, output = 0, []
+
         if not root:
-            return []
-        
-        level = 0
-        queue, output = deque([root]), []
+            return output
+
+        queue = deque([root])
 
         while queue:
+            # We need to add nodes at idx level (e.g., for the root node, at idx = 0)
+            # So if the len(output) is equal to level, we need to add a new array to the output
             if len(output) == level:
                 output.append([])
+            
+            # How many elements are there to process at this level? The current length of the queue
+            # (e.g., at the root node, there is only one element to process; next, only n children of the root)
+            for _ in range(0, len(queue)):
+                # Get the next node from the queue
+                curr = queue.popleft()
+                # Append its value to the output array (at the appropriate level)
+                output[level].append(curr.val)
 
-            nodes_at_level = len(queue)
-            for _ in range(0, nodes_at_level):
-                node = queue.popleft()
-                output[level].append(node.val)
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
+                # Add each of its children to the back of the queue (these won't be processed at this level!)
+                if curr.left:
+                    queue.append(curr.left)
+                if curr.right:
+                    queue.append(curr.right)
                 
+            # At this point, we've reached the next level
             level += 1
-
+        
         return output
