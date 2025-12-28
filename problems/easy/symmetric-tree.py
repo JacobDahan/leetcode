dc8c33efc08dd0b7219adce3358e7128c2b33499
Jacob Dahan <jbdahan@gmail.com>
2025-12-27 18:17:56 -0800

    Refactored: 101. Symmetric Tree

--- problems/easy/symmetric-tree.py
+++ problems/easy/symmetric-tree.py
@@ -1,5 +1,3 @@
-from collections import deque
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -7,63 +5,65 @@ from collections import deque
 #         self.left = left
 #         self.right = right
 class Solution:
-    # Iterative solution
-    # def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-    #     if not root:
-    #         return True
-
-    #     # Thoughts:
-    #     # - A tree is a mirror of itself if and only if every level of the tree
-    #     #   is a mirror of itself
-    #     # - Literally, a level is a mirror of itself if the list of nodes can be reversed without
-    #     #   the level changing
-    #     # - Since we are comparing level-by-level, this is appropriate for BFS
-    #     # - Algorithm: Go one level at a time; if level.reversed() != level, return False; 
-    #     #   if BFS completes, return True
-
-    #     # BFS uses a queue data structure (first-in, first-out)
-    #     queue = deque([root])
-    #     while queue:
-    #         # We know how many elements belong to this level by checking the length of the queue here
-    #         # (e.g., at start, len(queue) == 1, then the n children of the root node, etc.)
-    #         level = []
-    #         for _ in range(0, len(queue)):
-    #             curr = queue.popleft()
-    #             if curr:
-    #                 level.append(curr.val)
-    #                 queue.append(curr.left)
-    #                 queue.append(curr.right)
-    #             else:
-    #                 level.append(None)
-            
-    #         if level != list(reversed(level)):
-    #             return False
-        
-    #     return True
-    
-    # Recursive solution
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        # Thoughts:
-        # - A tree is only a mirror of itself if the root node is a mirror of itself
-        # - The root node is a mirror of itself if its left side is a mirror of its right side
-        # - The left side is a mirror of the right side if its left side is equal to the right side's right side...
-        # - In other words:
-        #       isMirror(l_node, r_node) can be solved by recursively comparing the LHS of the l_node to the
-        #       RHS of the r_node
-        return self.isMirror(root.left, root.right)
+        # We can do this iteratively or recursively
+        # Iterative approach:
+        # - A tree is only symmetric if every "level" of the tree is mirrored
+        # - When we want to process level by level, we should always think BFS
+        # - In other words, search each level and build a list of nodes in that level;
+        #   if that list of nodes is not equal to its reverse, then return false, else process next level
+        # return self.isSymmetricIterative(root)
+        # Recursive approach:
+        # - A tree is only symmetric if every sub-tree is symmetric
+        # - That is, we can recursively check every node in the tree
+        # - A node is only symmetric if it has no children, or two children such that the LHS of the L child
+        #   is equal to the RHS of the R child, and vice versa
+        # - If any node is not symmetric, the tree may not be symmetric
+        return self.isSymmetricRecursive(root.left, root.right)
 
-    def isMirror(self, l_node: Optional[TreeNode], r_node: Optional[TreeNode]) -> bool:
+    def isSymmetricIterative(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+        
+        # BFS requires a queue data structure (first-in, first-out)
+        # We process each level one at a time, and validate that that level is a mirror of itself
+        # If at any point we find a level that is not symmetric, we return false
+        from collections import deque
+        queue = deque([root])
+
+        while queue:
+            level = []
+            # The number of nodes at any level of the queue can be determined simply by checking the queue len
+            # here (e.g., len == 1 at the root node, len == n_child_nodes for its children, etc.)
+            for _ in range(0, len(queue)):
+                curr = queue.popleft()
+
+                if not curr:
+                    level.append(None)
+                    continue
+                
+                level.append(curr.val)
+                queue.append(curr.left)
+                queue.append(curr.right)
+        
+            if level != level[::-1]:
+                return False
+        
+        return True
+                
+
+    def isSymmetricRecursive(self, l_node: Optional[TreeNode], r_node: Optional[TreeNode]) -> bool:
         if not l_node and not r_node:
-            # Easiest mirror: None == None
+            # This is the simplest mirror: None is a clear mirror of None!
             return True
         
         if not l_node or not r_node:
-            # If one is None and the other is not, this cannot be a mirror
+            # If only one child is None, we cannot have a symmetric node
             return False
         
         if l_node.val != r_node.val:
-            # If the two nodes are not equal, it cannot be a mirror
+            # If the two nodes themselves are not equal, don't bother checking children
             return False
         
-        # Two nodes are only mirror nodes if the LHS of left is equal to the RHS of right, and vice versa...
-        return self.isMirror(l_node.left, r_node.right) and self.isMirror(l_node.right, r_node.left)
+        return self.isSymmetricRecursive(l_node.left, r_node.right)\
+            and self.isSymmetricRecursive(l_node.right, r_node.left)
