fff3b8be35492dcd9cf40b422b13479426b97c4f
Jacob Dahan <jbdahan@gmail.com>
2025-12-31 16:52:42 -0800

    Solved: 622. Design Circular Queue

--- problems/medium/find-k-closest-elements.py
+++ problems/medium/find-k-closest-elements.py
@@ -5,48 +5,26 @@ class Solution:
     #   |a - x| == |b - x| and a < b
 
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        # Take the simplest case (k = 1):
-        # We can use binary search to find the nearest element to x in the array, specifically
-        # - if arr[mid] >= x and mid > 0 and abs(arr[mid] - x) >= abs(arr[mid - 1] - x) --> look left, we have better answer
-        # - elif arr[mid] >= x --> bring right and left to mid, this is our best answer
-        # - elif arr[mid] < x and mid < len(arr) - 1 and abs(arr[mid] - x) >= abs(arr[mid + 1] - x) --> look right, we have better answer
-        # - else --> bright left to right, this is our best answer
-
-        left, right = 0, len(arr) - 1
+        # This is a sorted array: Think binary search
+        # ... But we can't just search for an element, we are searching for a *slice*
+        # What is our search condition?
+        # We want to find the index i such that arr[i:i+k] gives us the minimum difference from x
+        # At any given index i, we can determine whether the arr[i:i+k] or arr[i+1:i+k+1] slice is "better"
+        # - If the former is a better solution, no values j, j >= k are valid (as it would imply we can't include i)
+        # - If the latter is a better solution, no values j, j <= i are valid (as it would imply we include i)
+        # - The "better" solution is just whichever is closer -- i, or k -- to x
+        # - But what is "better" when two are equal? The leftmost answer
+        left, right = 0, len(arr) - k # Left index can never be greater than this
         while left < right:
             mid = left + ((right - left) // 2)
-            curr = arr[mid]
-
-            if curr >= x and mid > 0 and abs(arr[mid] - x) >= abs(arr[mid - 1] - x):
-                # Look left; there is a better answer to our left
-                right = mid
-            elif curr >= x:
-                # Bring right and left to mid; this is our best answer
-                # (mid is either equal to zero, or the next smallest number is so much smaller that it is further)
-                right = left = mid
-            elif curr < x and (abs(arr[mid] - x) > abs(arr[mid + 1] - x) or arr[mid] == arr[mid + 1]):
-                # Look right; there is a better answer to our right
+            if x - arr[mid] > arr[mid + k] - x:
+                # In other words, if x is larger than arr[mid] than arr[mid + k] is larger than x,
+                # arr[mid + k] must be closer to x and we should look right
                 left = mid + 1
             else:
-                # Bring right and left to mid; this is our best answer
-                # (mid is closer to x than the next largest number)
-                right = left = mid
+                # Otherwise, x may be smaller than mid, or just closer to x than mid + k,
+                # so we should look left
+                right = mid
         
-        # L = R at this point, pointing to the closest value
-        # Next, we need to find the k closest neighbors
-        # We can simply "fan out" from this index, comparing L and R neighbors to find the next closest
-        while k > right - left + 1:
-            if left == 0:
-                # We can only grab from right
-                right += 1
-            elif right == len(arr) - 1:
-                # We can only grab from left
-                left -= 1
-            elif abs(arr[left - 1] - x) <= abs(arr[right + 1] - x):
-                # Left neighbor is closer
-                left -= 1
-            else:
-                # Right neighbor is closer
-                right += 1
-        
-        return arr[left:right + 1] # Include RHS
+        # L == R
+        return arr[left:left+k]
