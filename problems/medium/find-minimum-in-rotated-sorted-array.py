89acf1a34c0f0c9f82c87d2f175e49a5db2a3ec1
Jacob Dahan <jbdahan@gmail.com>
2026-01-10 14:32:00 -0800

    Resolved: 153. Find Minimum in Rotated Sorted Array

--- problems/medium/find-minimum-in-rotated-sorted-array.py
+++ problems/medium/find-minimum-in-rotated-sorted-array.py
@@ -1,44 +1,61 @@
 class Solution:
-    # Suppose an array of length n sorted in ascending order is rotated between 1 and n times.
-    # Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+    """
+    Suppose an array of length n sorted in ascending order is rotated between 1 and n times. 
+    For example, the array nums = [0,1,2,4,5,6,7] might become:
+
+        [4,5,6,7,0,1,2] if it was rotated 4 times.
+        [0,1,2,4,5,6,7] if it was rotated 7 times.
+
+    Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+
+    Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+
+    You must write an algorithm that runs in O(log n) time.
+    """
+        
     def findMin(self, nums: List[int]) -> int:
-        # brute force
-        # min = float('inf')
-        # for num in nums:
-        #     if num < min:
-        #         min = num
-        # return min
+        """
+        Observations:
+        - We are asked to SEARCH for the index of an element in an ordered array
+        - The array was once sorted before being rotated between 1 and n times
+        - When an array is rotated k times, it maintains the following property:
+            - For all elements [0, ..., n-1-k], the array remains sorted
+            - For all elements [n-k, ..., 0), the array remains sorted
+            - In other words: There are two sorted sub-arrays that comprise the greater array
+        - The minimum of the array is the original first element of the array, which is the first
+          element for which nums[-1] > nums[i]
 
-        # What do we know?
-        # - The array was once monotonically increasing
-        # - There exists at most 1 "pivot" point that divides two monotonically increasing arrays
-        
-        # Option 1: We can find the pivot point using binary search, then search for the min trivially
-        # Option 2: We can execute a modified binary search for the minimum
-        
-        # Option 2: How can we do this?
-        # - No matter what point in the list we select, we have a sorted array on at least one side
-        # - If the array is sorted on the right, it means that all the numbers to the right are greater than
-        #   - So we must look left if nums[R] > nums[i]
-        #   - i is still a valid answer!!!
-        # - Else (if the array is not sorted on the right), it means by definition that a smaller number
-        #   exists for some index j, i < j <= R
+        Approach:
+        - We are asked to SEARCH for an element meeting some predicate in an ordered array,
+          this suggests that we should use binary search (and, specifically, the boundary convergence
+          form of binary search)
+            - In boundary convergence, we look for either the first or last element to meet a given predicate
+            - Here, we want the FIRST element satisfying the predicate nums[i] < nums[-1]
+            - Therefore, we follow the general MINIMIZATION structure for binary search
+            - Our invariant is: We keep a search bounds [lo, hi], where all values i >= hi satisfy the condition,
+              and all values i < lo do not (lo and all values in the search bounds are not yet checked)
+        - How can we use binary search in a non-sorted array?
+            - If we are searching for an element at index i, we are guaranteed that either
+              all elements j, j > i are sorted (j in our search domain), or j < i, or both
+        - Binary search runs in O(log n) time because it enables us to cut down the search space by half
+          at each step (and O(1) space)
+        """
+        if not nums:
+            return -1
 
-        left, right = 0, len(nums) - 1
-        while left < right:
-            mid = left + ((right - left) // 2)
-            val = nums[mid]
-            # Check if this value is less than the rightmost value
-            # Recall, this used to be the greatest value
-            # nums[right] can only be greater than nums[mid] if mid...right is still sorted,
-            # meaning that mid is the smallest index on that interval
-            if val < nums[right]:
-                # mid is still a plausible answer!!!
-                right = mid
-            # If nums[mid] > nums[right], we know that nums[right] is either the minimum,
-            # or that some minimum (the original index 0) exists between mid...right
+        lo, hi = 0, len(nums) - 1
+        while lo < hi: # when lo == hi, we know (by our invariant definition) that we found the first element
+                       # that satisfies the predicate (nums[mid] < nums[hi])
+            midpoint = lo + (hi - lo) // 2 # No int overflow in Python, but good habits...
+
+            if nums[midpoint] < nums[hi]:
+                # this is a valid answer, but perhaps not the first value to satisfy the predicate
+                # per our invariant, all i >= hi must satisfy the predicate, so move hi
+                hi = midpoint
             else:
-                # we already know that nums[right] < nums[mid], so mid can never be the answer
-                left = mid + 1
-        
-        return nums[left]
+                # midpoint does not satisfy the predicate
+                # our invariant dictates that all values i that do not satisfy the predicate must be i < lo, so move lo
+                # and discard this value
+                lo = midpoint + 1
+
+        return nums[lo]
