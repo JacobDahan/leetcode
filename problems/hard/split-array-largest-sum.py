40da056b6cfaaabc484e8d4d95f31901f0d1c276
Jacob Dahan <jbdahan@gmail.com>
2026-01-24 17:13:52 -0800

    Resolved: 410. Split Array Largest Sum

--- problems/hard/split-array-largest-sum.py
+++ problems/hard/split-array-largest-sum.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Solution:
     """
     Given an integer array nums and an integer k, split nums into k non-empty subarrays such that the largest sum of any subarray is minimized.
@@ -10,72 +12,56 @@ class Solution:
     def splitArray(self, nums: List[int], k: int) -> int:
         """
         Task:
-        - Given an integer array numns and an integer k, we must split nums into k sub-arrays
-        - The goal is to minimize the larget sum of any of the k sub-arrays
-        - In other words, we find the most "balanced" division of sub-arrays
+        - We are given an integer array nums and an integer k
+        - Our task is to split nums into k subarrays such that the LARGEST SUM of any subarray is MINIMIZED
+        - A subarray must be a contiguous part of the array
 
         Observations:
-        - A sub-array is a contiguous part of the array, so forming sub-arrays is relatively trivial
-        - The minimum "maximum" that we may ever return is the maximum of the list
-            - That is, no minimum maximum sum can ever be smaller than taking the largest element and putting it in its own array
-        - The maximum maximum that we could ever return would be the sum of the elements in the list (put all the elements in one sub-array and call it a day)
-        - Therefore, we have a bounded answer space of [max(nums), sum(nums)]
-        - What can we do with this answer space?
-            - Well, for each answer, we could check if there's a valid solution! Starting at max(nums) and going up...
-            - We can define is_valid(minimized_max_sum) := [for each element in nums, add to current sub-array if curr_sum + element <= minimized_max_sum; else next sub-array]
-            - Once we find an answer, since we started at the "bottom end" of the answer space, we know it's the minimum!
-            - ... But what if the sum(nums) is *huge*? We can search more efficiently using binary search over the answer space!
+        - For any value l := largest_sum, it is trivial to evaluate if it is possible to split nums into k or fewer sub-arrays with largest sum l
+        - For any value l := largest_sum that can be satisfied, all values i, i > l MUST also satisfy the predicate
+        - For any value l := largest_sum that cannot be satisfied, all values i, i < l MUST NOT satisfy the predicate (think: If we can't split an array into chunks of size <= 10, how could we for 9?)
+        - The values largetst_sum are a continuous, naturally ordered, duplicate free array
+        - With these conditions, we can use binary search along the answer space to find the BOUNDARY CONVERGENCE POINT where the predicate STARTS TO BECOME SATISFIED (minimization)
 
-        Binary Search:
-        - Binary search allows us to discard half of the answer space at each step
-            - We know that for any "minimized_max_sum" satisfying the predicate, all values mms > minimized_max_sum will also satisfy our is_valid predicate
-                - i.e., if we can minimize the sums to 10, we can also minimize the sums to 100
-            - We know that for any "minimized_max_sum" NOT satisfying the predicate, all values mms < minimized_max_sum will also NOT satisfy our is_valid predicate
-                - i.e., if we cannot minimize the sums to 100, we cannot minimize the sums to 10
-            - With these conditions met, we can assertively keep or throw away half of the answer space with each is_valid check
-        - Traditionally, binary search is O(n) runtime (n = size of answer space), but here we need to iterate over nums at each loop, resulting in O(m log n) runtime
-          (where n is the *sum* of nums, since that defines our search range)
-        - This is still O(1) space
-
-        So how do we execute the binary search?
-        - This is a classic boundary search, minimization problem: We want to find the "least big" number that satisfies our predicate (first true)
-        - Predicate: For a given answer, can we create contiguous sub-arrays such that all of the sums are equal to or less than the answer?
-        - Invariant: For any i, i < lo, the predicate must not be satisfied; for any i, i >= hi, the predicate mut be satisfied
-        - At each step, if the answer is valid, we set hi = answer (keeping our invariant true), else we reject this answer and all answers lower than it from the answer space
+        Algorithm:
+        - largest_sum may not be smaller than the maximum of max(nums) (all elements must fit) and sum(nums) // k (perfect packing)
+        - largest_sum must satisfy the predicate where l = sum(nums) (we can of course split into arrays of size total_size or smaller!)
+        - Therefore, perform binary search along [sum(nums) // k, sum(nums)]
+        - Invariant: For any value i, i < lo, the predicate must not be satisfied; for any value i, i >= hi, the predicate must be satisfied
+        - Termination condition: When lo == hi, we have definitionally found our boundary condition -- the first value to satisfy the predicate
+        - Predicate: We can simply form contiguous sub-arrays up to a maximum sum of largest_sum, and return True if we can fit into k or fewer sub-arrays
         """
-        lo, hi = max(nums), sum(nums)
-        
-        def is_valid(answer: int) -> bool:
-            """
-            Utility method to determine if a given answer satisfies the following predicate:
-            Can the array nums be divided into contiguous sub-arrays such that no sum of any sub-array is greater than the answer?
-            """
-            running_sum, sub_array_count = answer, 0
-            for number in nums:
-                if running_sum - number < 0: # this value can't fit in this sub-array, move to the next
-                    sub_array_count += 1
-                    running_sum = answer
 
-                    if sub_array_count == k: # if we've exhausted all our sub-arrays, this is NOT a valid answer
-                        return False
+        lo, hi = max(sum(nums) // k, max(nums)), sum(nums)
 
-                running_sum -= number # this number CAN fit in this sub-array, guaranteed, because the sub-arrays are minimially the maximum size of nums
-            
-            return True # if we've made it here, we managed to fit all our numbers in the available arrays!
+        def can_split_with_sum_or_smaller(sum: int) -> bool:
+            sub_arrays_in_use = 1
+            sub_array_sum = 0
 
+            for num in nums:
+                if sub_array_sum + num > sum:
+                    # we'd overflow this sub-array by adding another element, so move to the next
+                    sub_arrays_in_use += 1
+                    sub_array_sum = 0
                 
-        
-        while lo < hi: # note we use lo < hi because when lo == hi, we've found our answer!
-            midpoint = lo + (hi - lo) // 2 # bias towards lower values (if mid == lo and is_valid, hi will be drawn down so we don't get stuck)
+                if sub_arrays_in_use > k:
+                    # exit early, we can't fit into sub-arrays of size sum
+                    return False
+                
+                sub_array_sum += num
+            
+            return True
 
-            if is_valid(midpoint):
-                # this is a valid answer, but may not be the MINIMUM (first true) answer, so we look "left"
-                # we keep this value, as it may be the answer, and set it to hi to maintain our invariant (all i >= hi satisfy predicate)
-                hi = midpoint
+        while lo < hi: # why not equals? our invariant guarantees that when lo == hi we've found the first value that satisfies the predicate
+            mid = lo + (hi - lo) // 2 # avoid int overflow, even though it's not possible in Python
+
+            if can_split_with_sum_or_smaller(mid):
+                # this value satisfies the predicate and may be our answer, but there MAY exist a better solution (smaller maximum sum)
+                # we know that any value > mid also satisfies this, so we set hi = mid to satisfy our invariant
+                hi = mid
             else:
-                # this is not a valid answer
-                # discard this answer and all answers lower than it, as those ALSO will not be valid answers (see our observations above)
-                lo = midpoint + 1
+                # this value does not satisfy the predicate and must not be our answer
+                # all smaller values ALSO must not be our answer, so discard those as well
+                lo = mid + 1
         
         return lo
-
